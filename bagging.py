c806e5561f95a69ffa446a5b14f291c90766009c
AlitaMcF <ai.xufang@qq.com>
2020-11-29 00:55:32 +0800

    update bagging

--- bagging.py
+++ bagging.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.ensemble import BaggingClassifier
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 
 data_train = pd.read_csv('./processed_data/train_5.csv')
@@ -29,7 +30,8 @@ y_train = train[:, -1]
 x_validate = validate[:, 0:-1]
 y_validate = validate[:, -1]
 
-clf = BaggingClassifier(base_estimator=KNeighborsClassifier(), n_estimators=50, bootstrap=True)
+clf = BaggingClassifier(base_estimator=MLPClassifier(hidden_layer_sizes=(20, 2), alpha=1e-5, random_state=1,
+                                                     max_iter=1000), n_estimators=50, bootstrap=True)
 clf.fit(x_train, y_train)
 y_validate_pred = clf.predict(x_validate)
 print('Accuracy score:', metrics.accuracy_score(y_validate, y_validate_pred))
@@ -38,11 +40,13 @@ print('Accuracy score:', metrics.accuracy_score(y_validate, y_validate_pred))
 # KNN, train_3 Accuracy score: 0.8298116298116298
 # KNN, train_4 Accuracy score: 0.8314496314496315
 # KNN, train_5 Accuracy score: 0.8384930384930385
+# MLP, train_5, Accuracy score: 0.8576576576576577
 
 train_all = np.array(data_train)
 x_train_all = train_all[:, 0:-1]
 y_train_all = train_all[:, -1]
-clf2 = BaggingClassifier(base_estimator=KNeighborsClassifier(), n_estimators=50, bootstrap=True)
+clf2 = BaggingClassifier(base_estimator=MLPClassifier(hidden_layer_sizes=(20, 2), alpha=1e-5, random_state=1,
+                                                      max_iter=1000), n_estimators=50, bootstrap=True)
 clf2.fit(x_train_all, y_train_all)
 test_pred = np.int_(clf2.predict(test))
 print(test_pred)
